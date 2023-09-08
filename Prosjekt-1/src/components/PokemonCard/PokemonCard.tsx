e6d3d472348ebe29937bbf448f6c041d21a0f544
Snorre Saether <snosaet@hotmail.com>
2023-09-08 17:53:05 +0200

    simple cardComponent. Needs work (#1)

---
+++ Prosjekt-1/src/components/PokemonCard/PokemonCard.tsx
@@ -0,0 +1,48 @@
+import "./PokemonCard.css";
+
+type PokemonCardProps = {
+  name: string;
+  type: string;
+  isFavorite: boolean;
+  imgURL: string;
+};
+
+export function PokemonCard({
+  name,
+  type,
+  isFavorite,
+  imgURL,
+}: PokemonCardProps) {
+  const imgAltText = name + "-sprite";
+
+  const pokemonSprite = (
+    <img className="pokemonSprite" src={imgURL} alt={imgAltText} />
+  );
+
+  const typePil = <span className="typePil">{type}</span>;
+
+  const nameTitle = <h1 className="nameTitle">{name}</h1>;
+
+  const favoriteIndicator = (
+    <button
+      type="button"
+      className={`favoriteButton ${
+        isFavorite ? "isFavorite" : "isNotFavorite"
+      }`}
+      //this is a placeholder for a component. All apperance changes to
+      //favIndicator will be done with useState
+    >
+      favorite
+    </button>
+  );
+
+  return (
+    <div className="cardContainer">
+      {pokemonSprite}
+      {typePil}
+      {nameTitle}
+      {favoriteIndicator}
+    </div>
+  );
+}
+export default PokemonCard;
