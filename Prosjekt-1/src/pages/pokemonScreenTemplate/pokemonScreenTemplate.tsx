d084d397857b2a0e0d074f8150a0db791c3232dd
unknown <bcfogale@stud.ntnu.no>
2023-09-12 12:37:48 +0200

    (#2) Created a template screen for detailed pokemon information as well as a css file for the page.

---
+++ Prosjekt-1/src/pages/pokemonScreenTemplate/pokemonScreenTemplate.tsx
@@ -0,0 +1,35 @@
+
+
+type PokemonScreenTemplateProps = {
+    name: string;
+    type: string;
+    height: number;
+    weight: number;
+    abilities: string[];
+    moves: string[];
+};
+
+const PokemonScreenTemplate = ({ name, type, height, weight, abilities, moves }: PokemonScreenTemplateProps) => {
+
+    return (
+        <>
+            <h1>{name}</h1>
+            <h2>{type}</h2>
+            <h3>{height}</h3>
+            <h3>{weight}</h3>
+            <h3>{abilities}</h3>
+            <h3>{moves}</h3>
+        </>
+    )
+};
+
+export default PokemonScreenTemplate;
+
+/*
+Name
+Type
+Height
+Weight
+Abilities
+Moves
+*/
