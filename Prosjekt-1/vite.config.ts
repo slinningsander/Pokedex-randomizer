727663d279d802d35fc724c902a57c7afaf7d186
Snorre Saether <snosaet@hotmail.com>
2023-09-20 10:38:57 +0200

    cnfg make Jest features compatible w. Vitest #26

--- Prosjekt-1/vite.config.ts
+++ Prosjekt-1/vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-//vite/client  reference is poorly documented by Vitest, but seems to be needed for our purposes
+//vite/client reference is poorly documented by Vitest, but seems to be needed for our purposes
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
@@ -13,6 +13,8 @@ export default defineConfig({
     globals: true,
     //defines testing environemnt for testing web-apps as:
     environment: 'jsdom',
+    //setup file for testing, which imports jest features to be used with vitest
+    setupFiles: './src/test/setup.ts',
     //allows for parsing css, so can test css properties if needed:
     css: true,
   },
