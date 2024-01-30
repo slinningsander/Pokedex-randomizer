34794fb5c8f71c742f569b7eb70774cc6b8cf6e1
Sander Garland Slinning <sandergs@stud.ntnu.no>
2024-01-30 18:31:23 +0100

    Update base URL in vite.config.ts

--- Prosjekt-1/vite.config.ts
+++ Prosjekt-1/vite.config.ts
@@ -8,7 +8,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project1',
+  base: '/',
   test: {
     //allows for use of Vitest methodes without import:
     globals: true,
